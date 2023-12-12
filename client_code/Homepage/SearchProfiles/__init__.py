4373a90e4b2da4adc294007b2d61e77de31176ab
mattmalek44@icloud.com <mattmalek44@icloud.com>
2023-12-12 00:25:25 +0000

    Edited Homepage.SearchProfiles.AdvancedSearch and Homepage.SearchProfiles

--- client_code/Homepage/SearchProfiles/__init__.py
+++ client_code/Homepage/SearchProfiles/__init__.py
@@ -24,4 +24,30 @@ class SearchProfiles(SearchProfilesTemplate):
     """This method is called when the user presses Enter in this text box"""
     self.repeating_profile_panel.items = anvil.server.call('search_profiles_by_name', self.search_box.text)
 
+  def advanced_search_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    search_dict = {
+      'min_gpa': 0,
+      'min_gpa_check': False,
+      'max_gpa': 5,
+      'max_gpa_check': False,
+      'min_act': 0,
+      'min_act_check': False,
+      'max_act': 36,
+      'max_act_check': False,
+      'min_sat': 0,
+      'min_sat_check': False,
+      'max_sat': 1600,
+      'max_sat_check': False,
+      'min_grade': 9,
+      'min_grade_check': False,
+      'max_grade': 12,
+      'max_grade_check': False,
+      'location': '',
+      'location_check': False,
+      'school': '',
+      'school_check': False,
+    }
+    search_clicked = alert()
+
 
